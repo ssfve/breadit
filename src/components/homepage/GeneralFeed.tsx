8e5ab820119b328d76e422c513a983efa380d774
Elvin Lin <ssfvessfve@163.com>
2024-04-16 00:40:54 +0800

    add change

--- src/components/homepage/GeneralFeed.tsx
+++ src/components/homepage/GeneralFeed.tsx
@@ -4,16 +4,17 @@ import { INFINITE_SCROLL_PAGINATION_RESULTS } from '@/config'
 
 const GeneralFeed = async () => {
   const posts = await db.post.findMany({
-    orderBy: {
-      createdAt: 'desc',
-    },
+    // orderBy: {
+      // createdAt: 'desc',
+    // },
     include: {
       votes: true,
       author: true,
       comments: true,
       subreddit: true,
     },
-    take: parseInt(INFINITE_SCROLL_PAGINATION_RESULTS.toString()), // 4 to demonstrate infinite scroll, should be higher in production
+    take: parseInt(INFINITE_SCROLL_PAGINATION_RESULTS.toString()), 
+    // 4 to demonstrate infinite scroll, should be higher in production
   })
 
   return <PostFeed initialPosts={posts} />
