a785c8dfb7df445c632097b125082a152099167d
Elvin Lin <ssfvessfve@163.com>
2024-04-19 23:21:42 +0800

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -68,4 +68,4 @@ export const authOptions: NextAuthOptions = {
   },
 }
 
-export const getAuthSession = async () => getServerSession(authOptions)
+export const getAuthSession = () => getServerSession(authOptions)
