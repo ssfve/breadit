a8aafa6326074943a7f411d948f4fa2092fcd9f3
Elvin Lin <ssfvessfve@163.com>
2024-04-19 20:05:01 +0800

    fix problem

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,40 +1,38 @@
-import CommentsSection from '@/components/CommentsSection'
-import EditorOutput from '@/components/EditorOutput'
-import PostVoteServer from '@/components/post-vote/PostVoteServer'
-import { buttonVariants } from '@/components/ui/Button'
-import { db } from '@/lib/db'
-import { redis } from '@/lib/redis'
-import { formatTimeToNow } from '@/lib/utils'
-import { CachedPost } from '@/types/redis'
-import { Post, User, Vote } from '@prisma/client'
-import { ArrowBigDown, ArrowBigUp, Loader2 } from 'lucide-react'
-import { notFound } from 'next/navigation'
-import { Suspense } from 'react'
+import CommentsSection from "@/components/CommentsSection";
+import EditorOutput from "@/components/EditorOutput";
+import PostVoteServer from "@/components/post-vote/PostVoteServer";
+import { buttonVariants } from "@/components/ui/Button";
+import { db } from "@/lib/db";
+import { redis } from "@/lib/redis";
+import { formatTimeToNow } from "@/lib/utils";
+import { CachedPost } from "@/types/redis";
+import { Post, User, Vote } from "@prisma/client";
+import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
 
 interface SubRedditPostPageProps {
   params: {
-    postId: string
-  }
+    postId: string;
+  };
 }
 
-export const dynamic = 'force-dynamic'
-export const fetchCache = 'force-no-store'
-let cachedPost: CachedPost | null = null
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+let cachedPost: CachedPost | null = null;
 
 const SubRedditPostPage = async ({ params }: SubRedditPostPageProps) => {
-  
-  console.log("SubRedditPostPage is called")
+  console.log("SubRedditPostPage is called");
   // we do not cache posts
-  if(!cachedPost){
-    cachedPost = (await redis.hget(
-      `post`, `${params.postId}`
-    )) as CachedPost
-  } 
+  if (!cachedPost) {
+    cachedPost = (await redis.hget(`post`, `${params.postId}`)) as CachedPost;
+  }
+  console.log("cachedPost is ", cachedPost);
   // console.log(cachedPost)
-  // console.log(cachedPost)
-  let post: (Post & { votes: Vote[]; author: User }) | null = null
+  let post: (Post & { votes: Vote[]; author: User }) | null = null;
 
   if (!cachedPost) {
+    console.log("start to call post.findFirst");
     post = await db.post.findFirst({
       where: {
         id: params.postId,
@@ -43,35 +41,37 @@ const SubRedditPostPage = async ({ params }: SubRedditPostPageProps) => {
         votes: true,
         author: true,
       },
-    })
+    });
+    console.log("return from post.findFirst");
 
     cachedPost = {
-      authorUsername: post?.author.username ?? '',
+      authorUsername: post?.author.username ?? "",
       content: JSON.stringify(post?.content),
-      id: post?.id ?? '',
-      title: post?.title ?? 'error loading post title',
+      id: post?.id ?? "",
+      title: post?.title ?? "error loading post title",
       currentVote: null,
       createdAt: post?.createdAt ?? new Date(),
-    }
+    };
 
     const cachePayload: CachedPost = {
-      authorUsername: post?.author.username ?? '',
+      authorUsername: post?.author.username ?? "",
       content: JSON.stringify(post?.content),
-      id: post?.id ?? '',
-      title: post?.title ?? 'error loading post title',
+      id: post?.id ?? "",
+      title: post?.title ?? "error loading post title",
       currentVote: null,
       createdAt: post?.createdAt ?? new Date(),
-    }
+    };
 
     console.log("post is ", post);
-    await redis.hset(`post:${post?.id}`, cachePayload) // Store the post data as a hash
+    await redis.hset(`post:${post?.id}`, cachePayload); // Store the post data as a hash
+    console.log("return from redis.hset");
   }
 
-  if (!post && !cachedPost) return notFound()
-
+  if (!post && !cachedPost) return notFound();
+  console.log("start redenring SubRedditPostPage");
   return (
     <div>
-      <div className='h-full flex flex-col sm:flex-row items-center sm:items-start justify-between'>
+      <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
         <Suspense fallback={<PostVoteShell />}>
           {/* @ts-expect-error server component */}
           <PostVoteServer
@@ -84,53 +84,54 @@ const SubRedditPostPage = async ({ params }: SubRedditPostPageProps) => {
                 include: {
                   votes: true,
                 },
-              })
+              });
             }}
           />
         </Suspense>
 
-        <div className='sm:w-0 w-full flex-1 bg-white p-4 rounded-sm'>
-          <p className='max-h-40 mt-1 truncate text-xs text-gray-500'>
-            Posted by u/{post?.author.username ?? cachedPost.authorUsername}{' '}
+        <div className="sm:w-0 w-full flex-1 bg-white p-4 rounded-sm">
+          <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
+            Posted by u/{post?.author.username ?? cachedPost.authorUsername}{" "}
             {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
           </p>
-          <h1 className='text-xl font-semibold py-2 leading-6 text-gray-900'>
+          <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
             {post?.title ?? cachedPost.title}
           </h1>
 
           <EditorOutput content={post?.content ?? cachedPost.content} />
           <Suspense
             fallback={
-              <Loader2 className='h-5 w-5 animate-spin text-zinc-500' />
-            }>
+              <Loader2 className="h-5 w-5 animate-spin text-zinc-500" />
+            }
+          >
             {/* @ts-expect-error Server Component */}
             <CommentsSection postId={post?.id ?? cachedPost.id} />
           </Suspense>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 function PostVoteShell() {
   return (
-    <div className='flex items-center flex-col pr-6 w-20'>
+    <div className="flex items-center flex-col pr-6 w-20">
       {/* upvote */}
-      <div className={buttonVariants({ variant: 'ghost' })}>
-        <ArrowBigUp className='h-5 w-5 text-zinc-700' />
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigUp className="h-5 w-5 text-zinc-700" />
       </div>
 
       {/* score */}
-      <div className='text-center py-2 font-medium text-sm text-zinc-900'>
-        <Loader2 className='h-3 w-3 animate-spin' />
+      <div className="text-center py-2 font-medium text-sm text-zinc-900">
+        <Loader2 className="h-3 w-3 animate-spin" />
       </div>
 
       {/* downvote */}
-      <div className={buttonVariants({ variant: 'ghost' })}>
-        <ArrowBigDown className='h-5 w-5 text-zinc-700' />
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigDown className="h-5 w-5 text-zinc-700" />
       </div>
     </div>
-  )
+  );
 }
 
-export default SubRedditPostPage
+export default SubRedditPostPage;
