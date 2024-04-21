05b82f4894f2511b22b05cbe915bd8702176b6cc
Elvin Lin <ssfvessfve@163.com>
2024-04-21 09:32:24 +0800

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,9 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
+  console.log("req is ", req)
   const token = await getToken({ req })
-
+  console.log("middleware token is ", token)
   if (!token) {
     return NextResponse.redirect(new URL('/sign-in', req.nextUrl))
   }
